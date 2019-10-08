1c0a81eeb524eea1154988a967c79a42bd812074
mahdihatami <iz.hatami@gmail.com>
2019-10-08 14:07:35 +0300

    setup up button for fragments of activity

--- app/src/main/java/com/tuga/konum/view/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/tuga/konum/view/ui/signup/SignupActivity.kt
@@ -1,19 +1,45 @@
 package com.tuga.konum.view.ui.signup
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.tuga.konum.R
+import kotlinx.android.synthetic.main.activity_signup.*
+
 
 class SignupActivity : AppCompatActivity() {
+    private val navController: NavController
+        get() = findNavController(R.id.nav_host_signup_fragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        // use label in nav_host for setting fragments title
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            toolbar.title = navController.currentDestination?.label
+        }
+
+        // add Up button
+        NavigationUI.setupWithNavController(
+            toolbar,
+            NavHostFragment.findNavController(nav_host_signup_fragment)
+        )
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        findNavController(R.id.nav_host_signup_fragment).navigateUp()
-//        return super.onSupportNavigateUp()
-//    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            NavHostFragment.findNavController(nav_host_signup_fragment)
+        )
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
 }
