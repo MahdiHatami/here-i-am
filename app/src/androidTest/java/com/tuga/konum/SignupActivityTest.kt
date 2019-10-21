e74ae1c8a4a07793ac212d41809100ecc4e7aacd
mahdihatami <iz.hatami@gmail.com>
2019-10-21 19:59:10 +0300

    fix test cases

--- app/src/androidTest/java/com/tuga/konum/SignupActivityTest.kt
+++ app/src/androidTest/java/com/tuga/konum/SignupActivityTest.kt
@@ -1,13 +1,9 @@
 package com.tuga.konum
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import org.junit.runner.RunWith
-
 /**
  * Large End-to-End test for signup module
  */
-@RunWith(AndroidJUnit4::class)
-@LargeTest
-class SignupActivityTest {
-}
+//@RunWith(AndroidJUnit4::class)
+//@LargeTest
+//class SignupActivityTest {
+//}
