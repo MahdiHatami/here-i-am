a09c7f4580241ad26fe85433ecfdc8fb7141cdf9
mahdihatami <iz.hatami@gmail.com>
2019-11-05 17:19:42 +0300

    wert

---
+++ app/src/sharedTest/java/com/tuga/konum/signup/PhoneNumberFragmentTest.kt
@@ -0,0 +1,85 @@
+package com.tuga.konum.signup
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.tuga.konum.DaggerTestApplicationRule
+import com.tuga.konum.R
+import com.tuga.konum.models.entity.User
+import com.tuga.konum.view.ui.signup.phone.PhoneNumberFragment
+import com.tuga.konum.view.ui.signup.profile.ProfileFragmentDirections
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.robolectric.annotation.TextLayoutMode
+
+/**
+ * Integration test for phone number fragment
+ */
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+@ExperimentalCoroutinesApi
+@TextLayoutMode(TextLayoutMode.Mode.REALISTIC)
+class PhoneNumberFragmentTest {
+
+  /**
+   * Sets up Dagger components for testing.
+   */
+  @get:Rule
+  val rule = DaggerTestApplicationRule()
+
+  private var user = User()
+
+  @Test
+  fun validUsername_navigateToLocationFragment() {
+    // GIVEN - on the profile screen
+    val navController = mock(NavController::class.java)
+    launchFragment(navController)
+
+    // WHEN - enter valid username length should navigate to location fragment
+    user.username = "Mahdi"
+    onView(withId(R.id.edtUsername)).perform(replaceText(user.username))
+    onView(withId(R.id.btnProfileNext)).perform(click())
+
+    // THEN - verify that we navigate to location fragment
+    verify(navController).navigate(
+      ProfileFragmentDirections.actionProfileFragmentToLocationPermissionFragment()
+    )
+  }
+
+  @Test
+  fun unValidPassword_couldDisableButton() {
+    // GIVEN - on the profile fragment screen
+    val navController = mock(NavController::class.java)
+    launchFragment(navController)
+
+    // WHEN = unValid username entered and click next
+    val unValidUsername = ""
+    onView(withId(R.id.edtPassword)).perform(replaceText(unValidUsername))
+
+    // THEN - verify the button is disabled
+    onView(withId(R.id.btnProfileNext))
+      .check(ViewAssertions.matches(CoreMatchers.not((ViewMatchers.isEnabled()))))
+
+  }
+
+  private fun launchFragment(navController: NavController?) {
+    val scenario = launchFragmentInContainer<PhoneNumberFragment>(null, R.style.MaterialTheme)
+    scenario.onFragment {
+      Navigation.setViewNavController(it.view!!, navController)
+    }
+  }
+}
