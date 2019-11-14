029ae8ec3b1ac3a6afea990ba5020d2eb8403a34
mahdihatami <iz.hatami@gmail.com>
2019-11-14 13:20:29 +0300

    fix binding issue

--- app/src/main/java/com/tuga/konum/view/ui/signup/locationPermission/LocationPermissionFragment.kt
+++ app/src/main/java/com/tuga/konum/view/ui/signup/locationPermission/LocationPermissionFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.tuga.konum.EventObserver
 import com.tuga.konum.R
 import com.tuga.konum.databinding.FragmentLocationPermissionBinding
@@ -18,12 +20,10 @@ import javax.inject.Inject
 
 class LocationPermissionFragment : DaggerFragment() {
 
-  private val REQUEST_CODE_ACCESS_FINE_LOCATION: Int = 100
-
   @Inject
   lateinit var viewModelFactory: ViewModelProvider.Factory
 
-  private lateinit var viewDataBinding: FragmentLocationPermissionBinding
+  private lateinit var binding: FragmentLocationPermissionBinding
 
   private val viewModel by viewModels<LocationPermissionViewModel> { viewModelFactory }
 
@@ -32,13 +32,10 @@ class LocationPermissionFragment : DaggerFragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    val root = inflater.inflate(R.layout.fragment_location_permission, container, false)
-    viewDataBinding = FragmentLocationPermissionBinding.bind(root).apply {
-      this.viewModel = viewModel
-    }
-    // Set the lifecycle owner to the lifecycle of the view
-    viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
-    return viewDataBinding.root
+    binding =
+      DataBindingUtil.inflate(inflater, R.layout.fragment_location_permission, container, false)
+    binding.viewModel = viewModel
+    return binding.root
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -51,12 +48,20 @@ class LocationPermissionFragment : DaggerFragment() {
       )
     )
 
-    viewModel.requestLocationPermissionEvent.observe(this, EventObserver {
+    viewModel.requestLocationPermissionEvent.observe(viewLifecycleOwner, EventObserver {
       ActivityCompat.requestPermissions(
         activity!!,
         arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
         REQUEST_CODE_ACCESS_FINE_LOCATION
       )
+    })
+
+    viewModel.navigateToCircleAction.observe(viewLifecycleOwner, EventObserver {
+      findNavController()
+        .navigate(
+          LocationPermissionFragmentDirections
+            .actionLocationPermissionFragmentToJoinCircleFragment()
+        )
     })
   }
 
@@ -68,21 +73,24 @@ class LocationPermissionFragment : DaggerFragment() {
     when (requestCode) {
       REQUEST_CODE_ACCESS_FINE_LOCATION -> {
         if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-          toast("permission granted")
+          viewModel.setLocationPermissionStatus(
+            PermissionManager().getPermissionStatus(
+              activity!!,
+              android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+          )
         } else {
           toast("permission denied")
         }
-        viewModel.setLocationPermissionStatus(
-          PermissionManager().getPermissionStatus(
-            activity!!,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-          )
-        )
 
       }
 
       else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+  }
+
+  companion object {
+    private const val REQUEST_CODE_ACCESS_FINE_LOCATION: Int = 100
   }
 
 }
--- app/src/main/java/com/tuga/konum/view/ui/signup/locationPermission/LocationPermissionViewModel.kt
+++ app/src/main/java/com/tuga/konum/view/ui/signup/locationPermission/LocationPermissionViewModel.kt
@@ -1,15 +1,19 @@
 package com.tuga.konum.view.ui.signup.locationPermission
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tuga.konum.Event
 import com.tuga.konum.models.entity.User
+import com.tuga.konum.permission.PermissionManager
 import com.tuga.konum.permission.PermissionStatus
 import com.tuga.konum.permission.PermissionStatus.CAN_ASK_PERMISSION
+import com.tuga.konum.permission.PermissionStatus.PERMISSION_GRANTED
+import timber.log.Timber
 import javax.inject.Inject
 
-class LocationPermissionViewModel @Inject constructor(): ViewModel() {
+class LocationPermissionViewModel @Inject constructor() : ViewModel() {
 
   private var locationPermissionStatus: PermissionStatus = CAN_ASK_PERMISSION
 
@@ -17,18 +21,22 @@ class LocationPermissionViewModel @Inject constructor(): ViewModel() {
   val requestLocationPermissionEvent: LiveData<Event<Unit>> = _requestLocationEvent
 
   // navigation
-  private val _navigateToCircle = MutableLiveData<Event<User>>()
-  val navigateToCircleAction: LiveData<Event<User>> = _navigateToCircle
-
+  private val _navigateToCircle = MutableLiveData<Event<Unit>>()
+  val navigateToCircleAction: LiveData<Event<Unit>> = _navigateToCircle
 
   fun setLocationPermissionStatus(newPermissionStatus: PermissionStatus) {
+    Timber.d(newPermissionStatus.toString())
     if (locationPermissionStatus !== newPermissionStatus) {
       locationPermissionStatus = newPermissionStatus
     }
   }
 
   fun locationPermissionOnClick() {
-    _requestLocationEvent.value = Event(Unit)
+    if (locationPermissionStatus == PERMISSION_GRANTED) {
+      _navigateToCircle.value = Event(Unit)
+    } else {
+      _requestLocationEvent.value = Event(Unit)
+    }
   }
 
 }
