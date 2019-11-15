829a24233f770aad13ee93ddb1cd189ac5d0165c
mahdihatami <iz.hatami@gmail.com>
2019-11-15 11:45:33 +0300

    refactoring

--- app/src/main/java/com/tuga/konum/view/ui/signup/phone/PhoneNumberFragment.kt
+++ app/src/main/java/com/tuga/konum/view/ui/signup/phone/PhoneNumberFragment.kt
@@ -1,25 +1,9 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.tuga.konum.view.ui.signup.phone
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -33,9 +17,6 @@ import kotlinx.android.synthetic.main.fragment_phone_number.ccp
 import kotlinx.android.synthetic.main.fragment_phone_number.edtPhoneNumber
 import javax.inject.Inject
 
-/**
- * Main UI for the add task screen. Users can enter a task title and description.
- */
 class PhoneNumberFragment : DaggerFragment() {
 
   @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -69,7 +50,7 @@ class PhoneNumberFragment : DaggerFragment() {
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
-    viewModel.navigateToPasswordAction.observe(this, EventObserver { user ->
+    viewModel.navigateToPasswordAction.observe(viewLifecycleOwner, EventObserver { user ->
       findNavController()
         .navigate(
           PhoneNumberFragmentDirections.actionPhoneNumberFragmentToPasswordFragment(
