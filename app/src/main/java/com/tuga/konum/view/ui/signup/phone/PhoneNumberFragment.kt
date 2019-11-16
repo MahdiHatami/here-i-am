f5420e5136c3b0840ec5a20e4302f8669732f4c9
mahdihatami <iz.hatami@gmail.com>
2019-11-16 17:40:39 +0300

    impl sms listener

--- app/src/main/java/com/tuga/konum/view/ui/signup/phone/PhoneNumberFragment.kt
+++ app/src/main/java/com/tuga/konum/view/ui/signup/phone/PhoneNumberFragment.kt
@@ -1,36 +1,25 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.tuga.konum.view.ui.signup.phone
 
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.tuga.konum.EventObserver
 import com.tuga.konum.R
 import com.tuga.konum.databinding.FragmentPhoneNumberBinding
 import com.tuga.konum.extension.onTextChanged
+import com.tuga.konum.util.AppSignatureHelper
+import com.tuga.konum.util.SMSBroadcastReceiver
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_phone_number.ccp
 import kotlinx.android.synthetic.main.fragment_phone_number.edtPhoneNumber
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -39,6 +28,8 @@ import javax.inject.Inject
 class PhoneNumberFragment : DaggerFragment() {
 
   @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+  private val smsBroadcastReceiver by lazy { SMSBroadcastReceiver() }
 
   private lateinit var binding: FragmentPhoneNumberBinding
   private val viewModel by viewModels<PhoneNumberViewModel> { viewModelFactory }
@@ -64,12 +55,42 @@ class PhoneNumberFragment : DaggerFragment() {
       viewModel.onPhoneNumberChanged(it.toString())
     }
 
+    val helper = AppSignatureHelper(activity!!)
+    val sig = helper.getApplicationSignature()
+    sig.forEach {
+      Timber.d(it)
+    }
+
+    val client = SmsRetriever.getClient(activity!!)
+    val retriever = client.startSmsRetriever()
+    retriever.addOnSuccessListener {
+      val listener = object : SMSBroadcastReceiver.Listener {
+        override fun onSMSReceived(otp: String) {
+          // send data to server
+          Timber.i("sms: $otp")
+        }
+
+        override fun onTimeOut() {
+          Timber.d("timeout")
+        }
+      }
+      smsBroadcastReceiver.injectListener(listener)
+      activity?.registerReceiver(
+        smsBroadcastReceiver,
+        IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+      )
+    }
+
+    retriever.addOnFailureListener {
+      Timber.d("onViewCreated: addOnFailureListener")
+    }
+
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
-    viewModel.navigateToPasswordAction.observe(this, EventObserver { user ->
+    viewModel.navigateToPasswordAction.observe(viewLifecycleOwner, EventObserver { user ->
       findNavController()
         .navigate(
           PhoneNumberFragmentDirections.actionPhoneNumberFragmentToPasswordFragment(
@@ -77,6 +98,11 @@ class PhoneNumberFragment : DaggerFragment() {
           )
         )
     })
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    activity?.unregisterReceiver(smsBroadcastReceiver)
   }
 
 }
