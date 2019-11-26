5e2fe095ede26ef89c28723e4e008f22996a7c07
mahdihatami <iz.hatami@gmail.com>
2019-11-26 11:11:49 +0300

    asdf

--- app/src/main/java/com/tuga/konum/api/client/AuthClient.kt
+++ app/src/main/java/com/tuga/konum/api/client/AuthClient.kt
@@ -1,3 +1,17 @@
 package com.tuga.konum.api.client
 
-class AuthClient()
+import com.tuga.konum.api.ApiResponse
+import com.tuga.konum.api.async
+import com.tuga.konum.api.service.AuthService
+import com.tuga.konum.models.network.BooleanResponse
+import com.tuga.konum.models.network.UserDto
+
+class AuthClient(private val service: AuthService) {
+
+  fun getVerificationCode(
+    userDto: UserDto,
+    onResult: (response: ApiResponse<BooleanResponse>) -> Unit
+  ) {
+    service.getVerificationCode(userDto).async(onResult)
+  }
+}
--- app/src/main/java/com/tuga/konum/api/service/AuthService.kt
+++ app/src/main/java/com/tuga/konum/api/service/AuthService.kt
@@ -1,6 +1,8 @@
 package com.tuga.konum.api.service
 
+import androidx.annotation.BoolRes
 import com.tuga.konum.api.ApiResponse
+import com.tuga.konum.models.network.BooleanResponse
 import com.tuga.konum.models.network.UserDto
 import retrofit2.Call
 import retrofit2.http.Body
@@ -9,7 +11,7 @@ import retrofit2.http.POST
 interface AuthService {
 
   @POST("/api/login/CreateApplicant")
-  fun getVerificationCode(@Body userDto: UserDto): Call<ApiResponse<String>>
+  fun getVerificationCode(@Body userDto: UserDto): Call<ApiResponse<BooleanResponse>>
 
   @POST("/api/login/CheckVerificationCode")
   fun checkVerificationCode(@Body userDto: UserDto): Call<ApiResponse<String>>
