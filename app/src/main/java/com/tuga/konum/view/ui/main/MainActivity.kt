305deaa5b8e91315f06f5cfc393bae5f59978683
mahdihatami <iz.hatami@gmail.com>
2019-12-10 15:15:36 +0300

    setup google map and create main activity and new navHost

--- app/src/main/java/com/tuga/konum/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/tuga/konum/view/ui/main/MainActivity.kt
@@ -2,18 +2,35 @@ package com.tuga.konum.view.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.tuga.konum.R
 
 class MainActivity : AppCompatActivity() {
 
+  private val navController: NavController
+    get() = findNavController(R.id.nav_host_main_fragment)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.main_activity)
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-          .replace(R.id.container, MainFragment.newInstance())
-          .commitNow()
-    }
+    setContentView(R.layout.activity_main)
   }
 
+  override fun onSupportNavigateUp(): Boolean {
+    return navController.navigateUp()
+  }
+
+  override fun onBackPressed() {
+
+    when (navController.currentDestination?.id) {
+      R.id.peopleFragment -> {
+        finish()
+        moveTaskToBack(true)
+      }
+      else -> {
+        super.onBackPressed()
+      }
+    }
+
+  }
 }
