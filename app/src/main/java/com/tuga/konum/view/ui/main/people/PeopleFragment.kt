68863d44e541644012c8df16fe7d5f48308f9035
mahdihatami <iz.hatami@gmail.com>
2019-12-20 22:53:32 +0300

    add custom marker initial files

--- app/src/main/java/com/tuga/konum/view/ui/main/people/PeopleFragment.kt
+++ app/src/main/java/com/tuga/konum/view/ui/main/people/PeopleFragment.kt
@@ -1,21 +1,31 @@
 package com.tuga.konum.view.ui.main.people
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import androidx.annotation.DrawableRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.tuga.konum.R
 import com.tuga.konum.databinding.FragmentPeopleBinding
 import dagger.android.support.DaggerFragment
+import de.hdodenhof.circleimageview.CircleImageView
 import javax.inject.Inject
+
 
 class PeopleFragment : DaggerFragment() {
 
@@ -46,9 +56,48 @@ class PeopleFragment : DaggerFragment() {
   }
 
   private val callback = OnMapReadyCallback { googleMap ->
-    val sydney = LatLng(38.3554, 38.3335)
-    googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
+    val malatya = LatLng(38.3554, 38.3335)
+//    googleMap.addMarker(MarkerOptions().position(malatya).title("Marker in Sydney"))
+
+    googleMap.addMarker(
+      MarkerOptions()
+        .position(malatya)
+        .icon(
+          BitmapDescriptorFactory.fromBitmap(
+            createCustomMarker(requireActivity(), R.drawable.avatar_0, "Manish")
+          )
+        )
+    ).title = "iPragmatech Solutions Pvt Lmt";
+
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(malatya, 15f))
+
+  }
+
+  private fun createCustomMarker(
+    context: Context, @DrawableRes resource: Int, _name: String?
+  ): Bitmap? {
+    val marker: View =
+      (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+        R.layout.custom_marker_layout,
+        null
+      )
+    val markerImage =
+      marker.findViewById<View>(R.id.user_dp) as CircleImageView
+    markerImage.setImageResource(resource)
+    val displayMetrics = DisplayMetrics()
+    (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+    marker.layoutParams = LayoutParams(52, LayoutParams.WRAP_CONTENT)
+    marker.measure(displayMetrics.widthPixels, displayMetrics.heightPixels)
+    marker.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels)
+    marker.buildDrawingCache()
+    val bitmap: Bitmap = Bitmap.createBitmap(
+      marker.measuredWidth,
+      marker.measuredHeight,
+      Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    marker.draw(canvas)
+    return bitmap
   }
 
 }
