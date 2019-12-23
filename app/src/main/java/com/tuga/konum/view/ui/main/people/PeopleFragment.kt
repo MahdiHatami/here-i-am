bfcd41d35a9e82a4c9dd8ca7c08939f20f967e4b
mahdihatami <iz.hatami@gmail.com>
2019-12-23 17:52:50 +0300

    add function to cache bitmap

--- app/src/main/java/com/tuga/konum/view/ui/main/people/PeopleFragment.kt
+++ app/src/main/java/com/tuga/konum/view/ui/main/people/PeopleFragment.kt
@@ -4,8 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +21,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -25,7 +30,6 @@ import com.tuga.konum.databinding.FragmentPeopleBinding
 import dagger.android.support.DaggerFragment
 import de.hdodenhof.circleimageview.CircleImageView
 import javax.inject.Inject
-
 
 class PeopleFragment : DaggerFragment() {
 
@@ -56,7 +60,7 @@ class PeopleFragment : DaggerFragment() {
   }
 
   private val callback = OnMapReadyCallback { googleMap ->
-    val malatya = LatLng(38.3554, 38.3335)
+    val malatya = LatLng(38.3754, 38.3335)
 //    googleMap.addMarker(MarkerOptions().position(malatya).title("Marker in Sydney"))
 
     googleMap.addMarker(
@@ -64,13 +68,44 @@ class PeopleFragment : DaggerFragment() {
         .position(malatya)
         .icon(
           BitmapDescriptorFactory.fromBitmap(
-            createCustomMarker(requireActivity(), R.drawable.avatar_0, "Manish")
+            createCustomMarker(requireActivity(), R.drawable.avatar_2, "Manish")
           )
         )
     ).title = "iPragmatech Solutions Pvt Lmt";
 
     googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(malatya, 15f))
 
+  }
+
+  fun iconCache(iconResId: Int): BitmapDescriptor? {
+    val iconCache: SparseArray<BitmapDescriptor> = SparseArray()
+    var icon = iconCache.get(iconResId)
+    if (icon == null) {
+      var drawable: Drawable
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        drawable = resources.getDrawable(iconResId, null)
+      } else {
+        drawable = resources.getDrawable(iconResId)
+      }
+      if (drawable is GradientDrawable) {
+        val bitmap: Bitmap = Bitmap.createBitmap(
+          drawable.getIntrinsicWidth(),
+          drawable.getIntrinsicHeight(),
+          Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, bitmap.width, bitmap.height)
+        drawable.draw(canvas)
+        icon = BitmapDescriptorFactory.fromBitmap(bitmap);
+        bitmap.recycle();
+      } else {
+        icon = BitmapDescriptorFactory.fromResource(iconResId);
+      }
+      iconCache.put(iconResId, icon);
+
+      return icon
+    }
+    return null
   }
 
   private fun createCustomMarker(
